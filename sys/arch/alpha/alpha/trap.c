418ff63df6098560924a14c58fdcbcce1e2a55cd
Jason Thorpe <thorpej@me.com>
2019-02-24 14:41:31 -0800

    _intrsafe is gone.

--- sys/arch/alpha/alpha/trap.c
+++ sys/arch/alpha/alpha/trap.c
@@ -121,8 +121,6 @@ static int unaligned_fixup(u_long, u_long, u_long, struct lwp *);
 static int handle_opdec(struct lwp *l, u_long *ucodep);
 static int alpha_ucode_to_ksiginfo(u_long ucode);
 
-extern int ufetchstoreerr_intrsafe(void);		/* MAGIC */
-
 /*
  * Initialize the trap vectors for the current processor.
  */
@@ -418,17 +416,6 @@ trap(const u_long a0, const u_long a1, const u_long a2, const u_long entry,
 					 * must be wired).
 					 */
 					goto dopanic;
-				}
-
-				/*
-				 * If this was an access caused by an
-				 * intrsafe ufetch/ustore, bail out now.
-				 */
-				if (onfault == (vaddr_t)ufetchstoreerr_intrsafe
-				    && pcb->pcb_accessaddr == a0) {
-					framep->tf_regs[FRAME_PC] = onfault;
-					pcb->pcb_onfault = 0;
-					goto out;
 				}
 
 				/*
