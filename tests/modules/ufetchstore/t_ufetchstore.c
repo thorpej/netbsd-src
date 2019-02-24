e35309a7208af092273a91a19e4b1df2489d1207
Jason Thorpe <thorpej@me.com>
2019-02-24 13:06:47 -0800

    Add a test harness to exercise the non-intrsafe fetch / store entry points.

---
+++ tests/modules/ufetchstore/t_ufetchstore.c
@@ -0,0 +1,525 @@
+/* $NetBSD$ */
+
+/*
+ * Copyright (c) 2019 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * This code is derived from software contributed to The NetBSD Foundation
+ * by Jason R. Thorpe.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__COPYRIGHT("@(#) Copyright (c) 2019\
+ The NetBSD Foundation, inc. All rights reserved.");
+__RCSID("$NetBSD$");
+
+#include <sys/types.h>
+#include <sys/endian.h>
+#include <sys/sysctl.h>
+
+#include <err.h>
+#include <errno.h>
+#include <limits.h>
+
+#include <atf-c.h>
+
+/* MUST BE KEPT IN SYNC WITH MODULE */
+struct ufetchstore_test_args {
+	void		*uaddr;
+	bool		is_store;
+	int		size;
+	int		fetchstore_error;
+	union {
+		uint8_t	 val8;
+		uint16_t val16;
+		uint32_t val32;
+#ifdef _LP64
+		uint64_t val64;
+#endif
+	};
+};
+
+#define	mib_name	"kern.ufetchstore_test.test"
+
+static int
+do_sysctl(struct ufetchstore_test_args *args)
+{
+	int rv;
+
+	args->fetchstore_error = EBADF;	/* poison */
+
+	/*
+	 * Yes, the intent is to provide the pointer, not the structure,
+	 * to the kernel side of the test harness.
+	 */
+	rv = sysctlbyname(mib_name, NULL, NULL, &args, sizeof(args));
+	if (rv != 0) {
+		rv = errno;
+		warn("sysctlbyname('%s') -> %d", mib_name, rv);
+		return rv;
+	}
+	return 0;
+}
+
+static int
+do_ufetch_8(const uint8_t *uaddr, uint8_t *res)
+{
+	struct ufetchstore_test_args args = {
+		.uaddr = __UNCONST(uaddr),
+		.is_store = false,
+		.size = 8,
+	};
+
+	ATF_REQUIRE_EQ(do_sysctl(&args), 0);
+	*res = args.val8;
+	return args.fetchstore_error;
+}
+
+static int
+do_ufetch_16(const uint16_t *uaddr, uint16_t *res)
+{
+	struct ufetchstore_test_args args = {
+		.uaddr = __UNCONST(uaddr),
+		.is_store = false,
+		.size = 16,
+	};
+
+	ATF_REQUIRE_EQ(do_sysctl(&args), 0);
+	*res = args.val16;
+	return args.fetchstore_error;
+}
+
+static int
+do_ufetch_32(const uint32_t *uaddr, uint32_t *res)
+{
+	struct ufetchstore_test_args args = {
+		.uaddr = __UNCONST(uaddr),
+		.is_store = false,
+		.size = 32,
+	};
+
+	ATF_REQUIRE_EQ(do_sysctl(&args), 0);
+	*res = args.val32;
+	return args.fetchstore_error;
+}
+
+#ifdef _LP64
+static int
+do_ufetch_64(const uint64_t *uaddr, uint64_t *res)
+{
+	struct ufetchstore_test_args args = {
+		.uaddr = __UNCONST(uaddr),
+		.is_store = false,
+		.size = 64,
+	};
+
+	ATF_REQUIRE_EQ(do_sysctl(&args), 0);
+	*res = args.val64;
+	return args.fetchstore_error;
+}
+#endif /* _LP64 */
+
+static int
+do_ustore_8(uint8_t *uaddr, uint8_t val)
+{
+	struct ufetchstore_test_args args = {
+		.uaddr = uaddr,
+		.is_store = true,
+		.size = 8,
+		.val8 = val,
+	};
+
+	ATF_REQUIRE_EQ(do_sysctl(&args), 0);
+	return args.fetchstore_error;
+}
+
+static int
+do_ustore_16(uint16_t *uaddr, uint16_t val)
+{
+	struct ufetchstore_test_args args = {
+		.uaddr = uaddr,
+		.is_store = true,
+		.size = 16,
+		.val16 = val,
+	};
+
+	ATF_REQUIRE_EQ(do_sysctl(&args), 0);
+	return args.fetchstore_error;
+}
+
+static int
+do_ustore_32(uint32_t *uaddr, uint32_t val)
+{
+	struct ufetchstore_test_args args = {
+		.uaddr = uaddr,
+		.is_store = true,
+		.size = 32,
+		.val32 = val,
+	};
+
+	ATF_REQUIRE_EQ(do_sysctl(&args), 0);
+	return args.fetchstore_error;
+}
+
+#ifdef _LP64
+static int
+do_ustore_64(uint64_t *uaddr, uint64_t val)
+{
+	struct ufetchstore_test_args args = {
+		.uaddr = uaddr,
+		.is_store = true,
+		.size = 64,
+		.val64 = val,
+	};
+
+	ATF_REQUIRE_EQ(do_sysctl(&args), 0);
+	return args.fetchstore_error;
+}
+#endif /* _LP64 */
+
+struct memory_cell {
+	unsigned long guard0;
+	union {
+		unsigned long test_cell;
+#ifdef _LP64
+		uint64_t val64;
+#endif
+		uint32_t val32[sizeof(long) / 4];
+		uint16_t val16[sizeof(long) / 2];
+		uint8_t  val8 [sizeof(long)    ];
+	};
+	unsigned long guard1;
+};
+
+#define	index8		1
+#define	index16		1
+#define	index32		0
+
+#define	test_pattern8	0xa5
+#define	test_pattern16	0x5a5a
+#define	test_pattern32	0xb01cafe1
+#ifdef _LP64
+#define	test_pattern64	0xcafedeadfeedbabe
+#endif
+
+#define	test_cell_val8	((unsigned long)test_pattern8  << (index8  * NBBY))
+#define	test_cell_val16	((unsigned long)test_pattern16 << (index16 * NBBY*2))
+#define	test_cell_val32	((unsigned long)test_pattern32 << (index32 * NBBY*4))
+#ifdef _LP64
+#define	test_cell_val64	((unsigned long)test_pattern64)
+#endif
+
+#if _BYTE_ORDER == _LITTLE_ENDIAN
+#define	read_test_cell(cell)		(cell)->test_cell
+#define	write_test_cell(cell, v)	(cell)->test_cell = (v)
+#endif
+
+#if _BYTE_ORDER == _BIG_ENDIAN
+#ifdef _LP64
+#define	read_test_cell(cell)		bswap64((cell)->test_cell)
+#define	write_test_cell(cell, v)	(cell)->test_cell = bswap64(v)
+#else
+#define	read_test_cell(cell)		bswap32((cell)->test_cell)
+#define	write_test_cell(cell, v)	(cell)->test_cell = bswap32(v)
+#endif /* _LP64 */
+#endif /* _BYTE_ORDER == _BIG_ENDIAN */
+
+#define	memory_cell_initializer		\
+	{				\
+		.guard0 = ULONG_MAX,	\
+		.test_cell = 0,		\
+		.guard1 = ULONG_MAX,	\
+	}
+
+static bool
+memory_cell_check_guard(const struct memory_cell * const cell)
+{
+	return cell->guard0 == ULONG_MAX &&
+	       cell->guard1 == ULONG_MAX;
+}
+
+ATF_TC(ufetch_8);
+ATF_TC_HEAD(ufetch_8, tc)
+{
+	atf_tc_set_md_var(tc, "descr",
+	    "test for correct ufetch_8 behavior");
+}
+ATF_TC_BODY(ufetch_8, tc)
+{
+	struct memory_cell cell = memory_cell_initializer;
+	uint8_t res;
+
+	write_test_cell(&cell, test_cell_val8);
+	ATF_REQUIRE_EQ(do_ufetch_8(&cell.val8[index8], &res), 0);
+	ATF_REQUIRE(memory_cell_check_guard(&cell));
+	ATF_REQUIRE(res == test_pattern8);
+}
+
+ATF_TC(ufetch_16);
+ATF_TC_HEAD(ufetch_16, tc)
+{
+	atf_tc_set_md_var(tc, "descr",
+	    "test for correct ufetch_16 behavior");
+}
+ATF_TC_BODY(ufetch_16, tc)
+{
+	struct memory_cell cell = memory_cell_initializer;
+	uint16_t res;
+
+	write_test_cell(&cell, test_cell_val16);
+	ATF_REQUIRE_EQ(do_ufetch_16(&cell.val16[index16], &res), 0);
+	ATF_REQUIRE(memory_cell_check_guard(&cell));
+	ATF_REQUIRE(res == test_pattern16);
+}
+
+ATF_TC(ufetch_32);
+ATF_TC_HEAD(ufetch_32, tc)
+{
+	atf_tc_set_md_var(tc, "descr",
+	    "test for correct ufetch_32 behavior");
+}
+ATF_TC_BODY(ufetch_32, tc)
+{
+	struct memory_cell cell = memory_cell_initializer;
+	uint32_t res;
+
+	write_test_cell(&cell, test_cell_val32);
+	ATF_REQUIRE_EQ(do_ufetch_32(&cell.val32[index32], &res), 0);
+	ATF_REQUIRE(memory_cell_check_guard(&cell));
+	ATF_REQUIRE(res == test_pattern32);
+}
+
+#ifdef _LP64
+ATF_TC(ufetch_64);
+ATF_TC_HEAD(ufetch_64, tc)
+{
+	atf_tc_set_md_var(tc, "descr",
+	    "test for correct ufetch_64 behavior");
+}
+ATF_TC_BODY(ufetch_64, tc)
+{
+	struct memory_cell cell = memory_cell_initializer;
+	uint64_t res;
+
+	write_test_cell(&cell, test_cell_val64);
+	ATF_REQUIRE_EQ(do_ufetch_64(&cell.val64, &res), 0);
+	ATF_REQUIRE(memory_cell_check_guard(&cell));
+	ATF_REQUIRE(res == test_pattern64);
+}
+#endif /* _LP64 */
+
+ATF_TC(ufetch_8_null);
+ATF_TC_HEAD(ufetch_8_null, tc)
+{
+	atf_tc_set_md_var(tc, "descr",
+	    "test for correct ufetch_8 NULL pointer behavior");
+}
+ATF_TC_BODY(ufetch_8_null, tc)
+{
+	uint8_t res;
+
+	ATF_REQUIRE_EQ(do_ufetch_8(NULL, &res), EFAULT);
+}
+
+ATF_TC(ufetch_16_null);
+ATF_TC_HEAD(ufetch_16_null, tc)
+{
+	atf_tc_set_md_var(tc, "descr",
+	    "test for correct ufetch_16 NULL pointer behavior");
+}
+ATF_TC_BODY(ufetch_16_null, tc)
+{
+	uint16_t res;
+
+	ATF_REQUIRE_EQ(do_ufetch_16(NULL, &res), EFAULT);
+}
+
+ATF_TC(ufetch_32_null);
+ATF_TC_HEAD(ufetch_32_null, tc)
+{
+	atf_tc_set_md_var(tc, "descr",
+	    "test for correct ufetch_32 NULL pointer behavior");
+}
+ATF_TC_BODY(ufetch_32_null, tc)
+{
+	uint32_t res;
+
+	ATF_REQUIRE_EQ(do_ufetch_32(NULL, &res), EFAULT);
+}
+
+#ifdef _LP64
+ATF_TC(ufetch_64_null);
+ATF_TC_HEAD(ufetch_64_null, tc)
+{
+	atf_tc_set_md_var(tc, "descr",
+	    "test for correct ufetch_64 NULL pointer behavior");
+}
+ATF_TC_BODY(ufetch_64_null, tc)
+{
+	uint64_t res;
+
+	ATF_REQUIRE_EQ(do_ufetch_64(NULL, &res), EFAULT);
+}
+#endif /* _LP64 */
+
+ATF_TC(ustore_8);
+ATF_TC_HEAD(ustore_8, tc)
+{
+	atf_tc_set_md_var(tc, "descr",
+	    "test for correct ustore_8 behavior");
+}
+ATF_TC_BODY(ustore_8, tc)
+{
+	struct memory_cell cell = memory_cell_initializer;
+
+	ATF_REQUIRE_EQ(do_ustore_8(&cell.val8[index8], test_pattern8), 0);
+	ATF_REQUIRE(memory_cell_check_guard(&cell));
+	ATF_REQUIRE(read_test_cell(&cell) == test_cell_val8);
+}
+
+ATF_TC(ustore_16);
+ATF_TC_HEAD(ustore_16, tc)
+{
+	atf_tc_set_md_var(tc, "descr",
+	    "test for correct ustore_16 behavior");
+}
+ATF_TC_BODY(ustore_16, tc)
+{
+	struct memory_cell cell = memory_cell_initializer;
+
+	ATF_REQUIRE_EQ(do_ustore_16(&cell.val16[index16], test_pattern16), 0);
+	ATF_REQUIRE(memory_cell_check_guard(&cell));
+	ATF_REQUIRE(read_test_cell(&cell) == test_cell_val16);
+}
+
+ATF_TC(ustore_32);
+ATF_TC_HEAD(ustore_32, tc)
+{
+	atf_tc_set_md_var(tc, "descr",
+	    "test for correct ustore_32 behavior");
+}
+ATF_TC_BODY(ustore_32, tc)
+{
+	struct memory_cell cell = memory_cell_initializer;
+
+	ATF_REQUIRE_EQ(do_ustore_32(&cell.val32[index32], test_pattern32), 0);
+	ATF_REQUIRE(memory_cell_check_guard(&cell));
+	ATF_REQUIRE(read_test_cell(&cell) == test_cell_val32);
+}
+
+#ifdef _LP64
+ATF_TC(ustore_64);
+ATF_TC_HEAD(ustore_64, tc)
+{
+	atf_tc_set_md_var(tc, "descr",
+	    "test for correct ustore_64 behavior");
+}
+ATF_TC_BODY(ustore_64, tc)
+{
+	struct memory_cell cell = memory_cell_initializer;
+
+	ATF_REQUIRE_EQ(do_ustore_64(&cell.val64, test_pattern64), 0);
+	ATF_REQUIRE(memory_cell_check_guard(&cell));
+	ATF_REQUIRE(read_test_cell(&cell) == test_cell_val64);
+}
+#endif /* _LP64 */
+
+ATF_TC(ustore_8_null);
+ATF_TC_HEAD(ustore_8_null, tc)
+{
+	atf_tc_set_md_var(tc, "descr",
+	    "test for correct ustore_8 NULL pointer behavior");
+}
+ATF_TC_BODY(ustore_8_null, tc)
+{
+	ATF_REQUIRE_EQ(do_ustore_8(NULL, 0), EFAULT);
+}
+
+ATF_TC(ustore_16_null);
+ATF_TC_HEAD(ustore_16_null, tc)
+{
+	atf_tc_set_md_var(tc, "descr",
+	    "test for correct ustore_16 NULL pointer behavior");
+}
+ATF_TC_BODY(ustore_16_null, tc)
+{
+	ATF_REQUIRE_EQ(do_ustore_16(NULL, 0), EFAULT);
+}
+
+ATF_TC(ustore_32_null);
+ATF_TC_HEAD(ustore_32_null, tc)
+{
+	atf_tc_set_md_var(tc, "descr",
+	    "test for correct ustore_32 NULL pointer behavior");
+}
+ATF_TC_BODY(ustore_32_null, tc)
+{
+	ATF_REQUIRE_EQ(do_ustore_32(NULL, 0), EFAULT);
+}
+
+#ifdef _LP64
+ATF_TC(ustore_64_null);
+ATF_TC_HEAD(ustore_64_null, tc)
+{
+	atf_tc_set_md_var(tc, "descr",
+	    "test for correct ustore_64 NULL pointer behavior");
+}
+ATF_TC_BODY(ustore_64_null, tc)
+{
+	ATF_REQUIRE_EQ(do_ustore_64(NULL, 0), EFAULT);
+}
+#endif /* _LP64 */
+
+ATF_TP_ADD_TCS(tp)
+{
+	ATF_TP_ADD_TC(tp, ufetch_8);
+	ATF_TP_ADD_TC(tp, ufetch_16);
+	ATF_TP_ADD_TC(tp, ufetch_32);
+#ifdef _LP64
+	ATF_TP_ADD_TC(tp, ufetch_64);
+#endif
+
+	ATF_TP_ADD_TC(tp, ufetch_8_null);
+	ATF_TP_ADD_TC(tp, ufetch_16_null);
+	ATF_TP_ADD_TC(tp, ufetch_32_null);
+#ifdef _LP64
+	ATF_TP_ADD_TC(tp, ufetch_64_null);
+#endif
+
+	ATF_TP_ADD_TC(tp, ustore_8);
+	ATF_TP_ADD_TC(tp, ustore_16);
+	ATF_TP_ADD_TC(tp, ustore_32);
+#ifdef _LP64
+	ATF_TP_ADD_TC(tp, ustore_64);
+#endif
+
+	ATF_TP_ADD_TC(tp, ustore_8_null);
+	ATF_TP_ADD_TC(tp, ustore_16_null);
+	ATF_TP_ADD_TC(tp, ustore_32_null);
+#ifdef _LP64
+	ATF_TP_ADD_TC(tp, ustore_64_null);
+#endif
+
+	return atf_no_error();
+}
