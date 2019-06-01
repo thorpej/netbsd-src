5587545569ce4827cb2015df25ac77cde47fe1fb
Jason Thorpe <thorpej@me.com>
2019-05-31 19:35:29 -0700

    Add ethsys.

---
+++ sys/arch/arm/mediatek/mt2701_ethsys.c
@@ -0,0 +1,179 @@
+/* $NetBSD$ */
+
+/*-
+ * Copyright (c) 2019 Jason R. Thorpe
+ * Copyright (c) 2017 Jared McNeill <jmcneill@invisible.ca>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__KERNEL_RCSID(0, "$NetBSD$");
+
+#include <sys/param.h>
+#include <sys/bus.h>
+#include <sys/device.h>
+#include <sys/systm.h>
+
+#include <dev/fdt/fdtvar.h>
+
+#include <arm/mediatek/mtk_cru.h>
+#include <arm/mediatek/mt2701_ethsys.h>
+
+/* Relative to ETHSYS Base address */
+#define	ETHSYS_ID0_3			0x000
+#define	ETHSYS_ID4_7			0x004
+#define	ETHSYS_REVISION_ID		0x00c
+#define	ETHSYS_SYSCFG1			0x014
+#define	ETHSYS_CLKCFG0			0x02c
+#define	ETHSYS_CLKCFG1			0x030
+#define	ETHSYS_RSTCTL			0x034
+
+static int	mt2701_ethsys_match(device_t, cfdata_t, void *);
+static void	mt2701_ethsys_attach(device_t, device_t, void *);
+
+static const char * compatible[] = {
+	"mediatek,mt2701-ethsys",
+	NULL
+};
+
+struct mt2701_ethsys_softc {
+	struct mtk_cru_softc	sc_mtk_cru;
+};
+
+CFATTACH_DECL_NEW(mt2701_ethsys, sizeof(struct mt2701_ethsys_softc),
+    mt2701_ethsys_match, mt2701_ethsys_attach, NULL, NULL);
+
+static struct mtk_cru_reset mt2701_ethsys_resets[] = {
+	MTK_CRU_RESET(MT2701_RST_ETHSYS_SYS_RST, ETHSYS_RSTCTL, 0),
+	MTK_CRU_RESET(MT2701_RST_ETHSYS_MCM_RST, ETHSYS_RSTCTL, 2),
+	MTK_CRU_RESET(MT2701_RST_ETHSYS_FE_RST, ETHSYS_RSTCTL, 6),
+	MTK_CRU_RESET(MT2701_RST_ETHSYS_GMAC_RST, ETHSYS_RSTCTL, 23),
+	MTK_CRU_RESET(MT2701_RST_ETHSYS_PPE_RST, ETHSYS_RSTCTL, 31),
+};
+
+static const bus_size_t mt2701_ethsys_clkcfg1_regs[] = {
+	[MTK_CLK_GATE_REG_SET] = ETHSYS_CLKCFG1,
+	[MTK_CLK_GATE_REG_CLR] = ETHSYS_CLKCFG1,
+	[MTK_CLK_GATE_REG_STA] = ETHSYS_CLKCFG1,
+};
+
+static struct mtk_cru_clk mt2701_ethsys_clks[] = {
+	MTK_CLK_GATE(MT2701_CLK_ETHSYS_HSDMA, "hsdma_clk",
+	    "ethif_sel",
+	    mt2701_ethsys_clkcfg1_regs,
+	    __BIT(5),				/* enable */
+	    0),
+
+	MTK_CLK_GATE(MT2701_CLK_ETHSYS_ESW, "esw_clk",
+	    "ethpll_500m_ck",
+	    mt2701_ethsys_clkcfg1_regs,
+	    __BIT(6),				/* enable */
+	    0),
+
+	MTK_CLK_GATE(MT2701_CLK_ETHSYS_GP2, "gp2_clk",
+	    "trgpll",
+	    mt2701_ethsys_clkcfg1_regs,
+	    __BIT(7),				/* enable */
+	    0),
+
+	MTK_CLK_GATE(MT2701_CLK_ETHSYS_GP1, "gp1_clk",
+	    "ethpll_500m_ck",
+	    mt2701_ethsys_clkcfg1_regs,
+	    __BIT(8),				/* enable */
+	    0),
+
+	MTK_CLK_GATE(MT2701_CLK_ETHSYS_PCM, "pcm_clk",
+	    "ethif_sel",
+	    mt2701_ethsys_clkcfg1_regs,
+	    __BIT(11),				/* enable */
+	    0),
+
+	MTK_CLK_GATE(MT2701_CLK_ETHSYS_GDMA, "gdma_clk",
+	    "ethif_sel",
+	    mt2701_ethsys_clkcfg1_regs,
+	    __BIT(14),				/* enable */
+	    0),
+
+	MTK_CLK_GATE(MT2701_CLK_ETHSYS_I2S, "i2s_clk",
+	    "ethif_sel",
+	    mt2701_ethsys_clkcfg1_regs,
+	    __BIT(17),				/* enable */
+	    0),
+
+	MTK_CLK_GATE(MT2701_CLK_ETHSYS_CRYPTO, "crypto_clk",
+	    "ethif_sel",
+	    mt2701_ethsys_clkcfg1_regs,
+	    __BIT(29),				/* enable */
+	    0),
+};
+
+static int
+mt2701_ethsys_match(device_t parent, cfdata_t cf, void *aux)
+{
+	struct fdt_attach_args * const faa = aux;
+
+	return of_match_compatible(faa->faa_phandle, compatible);
+}
+
+static void
+mt2701_ethsys_attach(device_t parent, device_t self, void *aux)
+{
+	struct mt2701_ethsys_softc * const ethsys_sc = device_private(self);
+	struct mtk_cru_softc * const sc = &ethsys_sc->sc_mtk_cru;
+	struct fdt_attach_args * const faa = aux;
+	uint32_t val;
+	char id[9];
+
+	sc->sc_dev = self;
+	sc->sc_phandle = faa->faa_phandle;
+	sc->sc_bst = faa->faa_bst;
+
+	sc->sc_resets = mt2701_ethsys_resets;
+	sc->sc_nresets = __arraycount(mt2701_ethsys_resets);
+
+	sc->sc_clks = mt2701_ethsys_clks;
+	sc->sc_nclks = __arraycount(mt2701_ethsys_clks);
+
+	if (mtk_cru_attach(sc) != 0)
+		return;
+
+	val = CRU_READ(sc, ETHSYS_ID0_3);
+	id[0] =  val        & 0xff;
+	id[1] = (val >> 8)  & 0xff;
+	id[2] = (val >> 16) & 0xff;
+	id[3] = (val >> 24) & 0xff;
+
+	val = CRU_READ(sc, ETHSYS_ID4_7);
+	id[4] =  val        & 0xff;
+	id[5] = (val >> 8)  & 0xff;
+	id[6] = (val >> 16) & 0xff;
+	id[7] = (val >> 24) & 0xff;
+
+	id[8] = '\0';
+
+	aprint_naive("\n");
+	aprint_normal(": ETHSYS, ID='%s'\n", id);
+
+	mtk_cru_print(sc);
+}
---
+++ sys/arch/arm/mediatek/mt2701_ethsys.h
@@ -0,0 +1,55 @@
+/* $NetBSD$ */
+
+/*-
+ * Copyright (c) 2019 Jason R. Thorpe
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef _MEDIATEK_MT2701_ETHSYS_H_
+#define	_MEDIATEK_MT2701_ETHSYS_H_
+
+/*
+ * Reset IDs - must match DT bindings.
+ *
+ * These also happen to map to the RSTCTL bits.
+ */
+#define	MT2701_RST_ETHSYS_SYS_RST		0
+#define	MT2701_RST_ETHSYS_MCM_RST		2
+#define	MT2701_RST_ETHSYS_FE_RST		6
+#define	MT2701_RST_ETHSYS_GMAC_RST		23
+#define	MT2701_RST_ETHSYS_PPE_RST		31
+
+/*
+ * Clock IDs - must match DT bindings.
+ */
+#define	MT2701_CLK_ETHSYS_HSDMA			1
+#define	MT2701_CLK_ETHSYS_ESW			2
+#define	MT2701_CLK_ETHSYS_GP2			3
+#define	MT2701_CLK_ETHSYS_GP1			4
+#define	MT2701_CLK_ETHSYS_PCM			5
+#define	MT2701_CLK_ETHSYS_GDMA			6
+#define	MT2701_CLK_ETHSYS_I2S			7
+#define	MT2701_CLK_ETHSYS_CRYPTO		8
+
+#endif /* _MEDIATEK_MT2701_ETHSYS_H_ */
